76be8958fb72f882a3f9abfbb9816f33417cb11d
dongryeol <dongyeol01@naver.com>
2022-12-14 16:48:14 +0900

    작업 거의 완료 95%

--- server.ts
+++ server.ts
@@ -1,17 +1,45 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
-//@ts-ignore
-import SSE from "sse";
 
 const app = express();
+
+const port = process.env.PORT || 4000;
+
+const whitelist = ["http://localhost:3002", "https://chatgpt.algorithmlabs.io"];
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-const port = process.env.PORT || 4000;
+app.use(
+  cors({
+    origin: (requestOrigin: string | undefined, callback) => {
+      if (requestOrigin && whitelist.indexOf(requestOrigin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
+app.get("/models", async (_, res) => {
+  try {
+    const {
+      data: { data: list },
+    } = await openai.listModels({
+      headers: { Authorization: `Bearer ${process.env.OPENAI_API_KEY}` },
+    });
+    const data = list.sort((a, b) => b.created - a.created);
+    res.status(200).json({ data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+});
 
 app.get("/chat", async (req, res) => {
   try {
@@ -25,25 +53,19 @@ app.get("/chat", async (req, res) => {
 
     const completion = await openai.createCompletion(
       {
-        model: "text-davinci-003",
+        model: (req.query.model as string) || "text-davinci-003",
         prompt: req.query.msg as string,
         max_tokens: 2048,
         stream: true,
       },
-      { responseType: "stream" },
+      { responseType: "stream" }
     );
 
     let id = 1;
     //@ts-ignore
     completion.data.on("data", (data: Buffer) => {
-      // console.log(data.toString("utf-8"));
       res.write(`event: message\nid: ${id}\n${data.toString("utf-8")}`);
       id++;
-    });
-
-    res.on("close", () => {
-      console.log("close");
-      res.end();
     });
   } catch (error) {
     console.log(error);
@@ -52,5 +74,5 @@ app.get("/chat", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server is listening at localhost:${port}`);
+  console.log(`server is listening`);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,94 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
+
+import { ReactComponent as Send } from "icons/send.svg";
+
+type TMessageType = "ai" | "me";
+
+interface IMessage {
+  type: TMessageType;
+  value: string;
+}
+
+interface IModel {
+  created: number;
+  id: string;
+  object: string;
+  owned_by: string;
+  root: string;
+}
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [sendMessage, setSendMessage] = useState("");
-  const [value, setValue] = useState<string>("");
+  const [answer, setAnswer] = useState<string>("");
+  const [messageList, setMessageList] = useState<IMessage[]>([]);
+
+  const [modelList, setModelList] = useState<IModel[]>();
+  const [selectedModel, setSelectedModel] = useState<IModel>();
+
+  const [openSelect, setOpenSelect] = useState(false);
+
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const scrolltoBottom = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "auto" });
+    }
+  };
+
+  const setMessageListFn = (value: string, type: TMessageType) =>
+    setMessageList((prev) => [...prev, { value, type }]);
+
+  const onOpenSelectFn = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setOpenSelect(true);
+  };
 
   const onChangeSendMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.target;
-    setSendMessage(value);
+    if (value.trim().length === 0) {
+      setSendMessage("");
+    } else {
+      setSendMessage(value);
+    }
+  };
+
+  const onKeyDownFn = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter") {
+      onSend();
+    }
+  };
+
+  const onSelectModel = (e: React.MouseEvent<HTMLLIElement>, model: IModel) => {
+    e.stopPropagation();
+    setSelectedModel(model);
+    setOpenSelect(false);
   };
 
   const onSend = async () => {
     if (loading) return;
     if (!sendMessage) return;
     setLoading(true);
-    setValue("");
+    setMessageListFn(sendMessage, "me");
 
-    const source = new EventSource(
-      `http://localhost:4000/chat?msg=${encodeURIComponent(sendMessage)}`,
-    );
+    let eventUrl = `http://localhost:4000/chat?msg=${encodeURIComponent(
+      sendMessage
+    )}`;
+    if (selectedModel) {
+      eventUrl += `&model=${encodeURIComponent(selectedModel?.id)}`;
+    }
+
+    setSendMessage("");
+
+    const source = new EventSource(eventUrl);
 
     source.onmessage = (event) => {
       const parseData = JSON.parse(event.data);
       if (parseData?.choices?.[0]) {
         const data = parseData.choices[0];
-        console.log(data);
-        setValue((prev) => prev + data?.text || "");
+        setAnswer((prev) => prev + data?.text || "");
+        scrolltoBottom();
         if (data.finish_reason) {
           source.close();
           setLoading(false);
@@ -41,20 +103,71 @@ function App() {
     };
   };
 
+  useEffect(() => {
+    if (answer && !loading) {
+      setMessageListFn(answer, "ai");
+      setAnswer("");
+    }
+  }, [answer, loading]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/models").then((d) =>
+      d.json().then(({ data }) => {
+        setModelList(data);
+        setSelectedModel(data[0]);
+      })
+    );
+  }, []);
+
   return (
-    <div>
-      <main>
-        <div>{value}</div>
-        <div style={{ display: "flex" }}>
-          <textarea
-            // type="text"
-            value={sendMessage}
-            onChange={onChangeSendMessage}
-          />
-          <button onClick={onSend}>전송</button>
+    <main>
+      <header>
+        <h1>Demo Chat</h1>
+        <div className="custom-select" onClick={onOpenSelectFn}>
+          <span>{selectedModel?.id}</span>
+          {openSelect && (
+            <ul className="option-box">
+              {modelList?.map((model) => (
+                <li
+                  key={`model_${model.id}`}
+                  onClick={(e) => onSelectModel(e, model)}
+                >
+                  {model.id}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
-      </main>
-    </div>
+      </header>
+      <div className="scroll-box">
+        <ul>
+          {messageList.map(({ value, type }, i) => (
+            <li key={`message_${i}`} className={type}>
+              {value}
+            </li>
+          ))}
+          {answer && <li className="ai">{answer}</li>}
+          <div ref={scrollRef} />
+        </ul>
+      </div>
+
+      <div className="input-box">
+        <textarea
+          value={sendMessage}
+          onChange={onChangeSendMessage}
+          onKeyDown={onKeyDownFn}
+        />
+        <button onClick={onSend} disabled={loading}>
+          {loading ? (
+            <div className="stage">
+              <div className="dot-pulse"></div>
+            </div>
+          ) : (
+            <Send />
+          )}
+        </button>
+      </div>
+    </main>
   );
 }
 
